cc164b9c198d5b1927dbbbc9aec20ce8328246bf
Austin Peterson <austin@akpwebdesign.com>
2014-02-25 16:35:26 -0500

    Added Brick class.

---
+++ src/com/akpwebdesign/Breakout/entity/Brick.java
@@ -0,0 +1,14 @@
+package com.akpwebdesign.Breakout.entity;
+
+import org.newdawn.slick.Image;
+
+import com.akpwebdesign.Breakout.Game;
+
+public class Brick extends Entity {
+
+	public Brick(Image image, Game game) {
+		super(image, game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
